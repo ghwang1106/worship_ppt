292f5e30c05452f9878353ce6fa52309089a3498
Gyujoon Hwang <gyujoon.hwang@pennmedicine.upenn.edu>
2021-02-15 04:06:53 -0500

    bible2pptx first web version

--- worship_ppt/app.py
+++ worship_ppt/app.py
@@ -1,23 +1,38 @@
-from flask import Flask, render_template, send_file
+from bible2pptx_web import make_verse_ppt
+from flask import Flask, request, render_template, send_file
 from livereload import Server
 
+
 def create_app():
-  app = Flask(__name__)
+    app = Flask(__name__)
 
-  @app.route("/", methods=["GET"])
-  def index():
-    return render_template("index.html")
-  
-  @app.route('/download')
-  def downloadFile ():
-    path = "sample.txt"
-    return send_file(path, as_attachment=True)
+    @app.route("/", methods=["GET"])
+    def index():
+        return render_template("index.html")
 
+    @app.route('/download', methods=["GET", "POST"])
+    def downloadFile():
+        if request.method == "POST":
+            ppt_inputs = []
+            ppt_inputs.append(request.form['date_type'])
+            ppt_inputs.append(request.form['sermon_title'])
+            ppt_inputs.append(request.form['preacher'])
+            ppt_inputs.append(request.form['main_passage'])
+            ppt_inputs.append(request.form['quotes'])
+            try:
+                if request.form['action'] == 'Large PPT':
+                    path = make_verse_ppt(ppt_inputs)
+                    return send_file(path + '.pptx', as_attachment=True)
+                elif request.form['action'] == 'Verse PPT':
+                    path = make_verse_ppt(ppt_inputs, 1)
+                    return send_file(path + '.pptx', as_attachment=True)
+            except TypeError:
+                pass
 
-  return app
+    return app
 
 
 if __name__ == "__main__":
-  app = create_app()
-  server = Server(app.wsgi_app)
-  server.serve(debug=True)
+    app = create_app()
+    server = Server(app.wsgi_app)
+    server.serve(debug=True)
